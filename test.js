e3cc2f9260b0bb068d3507eaedbb2ba62a36d6d2
CF <cfreitas12345@gmail.com>
2023-01-24 12:35:45 -0700

    Redo version is functioning, needs style

--- test.js
+++ test.js
@@ -1,121 +1,270 @@
-// Code quiz
-// click start and and timer starts
-// Answer a question, then get another question
-// If wrong, time is subtracted
-// Ques ans/time reaches 0 = game over
-// Save my initials to the score
-// 
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
+// Directions***********************
+// This was a challenge where I had to create a multiple choice,
+// timed code quiz.  It had to run in the browser. It needed to
+// feature dynamically updated HTML and CSS, powered by JavaScript
+// code.  There needed to be clean, polished, and responsive user
+//interface.
 
 
-function startGame() {
-  start-btn.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+// The functions that it needed were:
+// 1) When the start button is clicked, the timer starts
+// when presented with a question.
+// 2) Points subtracted when there is a wrong answer.
+// 3) When either all questions are answered or when the timer
+// reaches 0, the game is over.
+// 4) When the game is over, initials and score are saved.
+
+
+// var
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+var questionDiv = document.getElementById("questionDiv");
+var questionTitle = document.getElementById("questionTitle");
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+var answerCheck = document.getElementById("answerCheck");
+
+var summary = document.getElementById("summary");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var initialInput = document.getElementById("initialInput");
+var everything = document.getElementById("everything");
+
+var highScoreSection = document.getElementById("highScoreSection");
+var finalScore = document.getElementById("finalScore");
+
+var goBackBtn = document.getElementById("goBackBtn");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
+var viewHighScore = document.getElementById("viewHighScore");
+var listOfHighScores = document.getElementById("listOfHighScores");
+
+// define other variables
+var correctAns = 0;
+var questionNum = 0;
+var scoreResult;
+var questionIndex = 0;
+
+ // Questions and answers
+ const questions = [
+  {
+      question: "Which of these is NOT a coding language?",
+      options: ['HTML','Code+','Python','JavaScript',],
+      answer: "Code+"
+  },
+  {
+      question: "What 2 possible values does Boolean have?",
+      options: ['a and b', 'Yes and no', 'True and false', 'Right and wrong',],
+      answer: "True and false"
+  },
+  {
+      question: "What does HTML stand for?'",
+      options: ['HyperType Made Language', 'Hex Text Made Language', 'Hex Type Made Language', 'HyperText Markup Language',],
+      answer: "HyperText Markup Language"
+  },
+  {
+      question:  "What does CSS stand for?'",
+      options: ['Copy Syntax Server','Copy Server Syntax','Cascading Style Server','Cascading Style Sheets',],
+      answer: "Cascading Style Sheets"
+  },
+  {
+      question: "Responsive design means to make a website look…",
+      options: ['good on any device and screen size.','good with added features and buttons.','clean and organized on the user interface.','function well, using a variety of different computer languages.',],
+      answer: "good on any device and screen size."
+  },
+  
+];
+
+// Timer
+var totalTime = 151;
+function newQuiz() {
+  questionIndex = 0;
+  totalTime = 150;
+  timeLeft.textContent = totalTime;
+  initialInput.textContent = "";
+
+  startDiv.style.display = "none";
+  questionDiv.style.display = "block";
+  timer.style.display = "block";
+  timesUp.style.display = "none";
+
+  var startTimer = setInterval(function() {
+      totalTime--;
+      timeLeft.textContent = totalTime;
+      if(totalTime <= 0) {
+          clearInterval(startTimer);
+          if (questionIndex < questions.length - 1) {
+              gameOver();
+          }
+      }
+  },1000);
+
+  showQuiz();
+};
+
+// console.log(questions[questionIndex].question);
+// console.log(questions[questionIndex].choices);
+
+// nextQuestion
+function showQuiz() {
+  nextQuestion();
 }
 
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+function nextQuestion() {
+  questionTitle.textContent = questions[questionIndex].question;
+  choiceA.textContent = questions[questionIndex].options[0];
+  choiceB.textContent = questions[questionIndex].options[1];
+  choiceC.textContent = questions[questionIndex].options[2];
+  choiceD.textContent = questions[questionIndex].options[3];
 }
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const btn = document.createElement('btn')
-    btn.innerText = answer.text
-    btn.classList.add('btn')
-    if (answer.correct) {
-      btn.dataset.correct = answer.correct
-    }
-    btn.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(btn)
-  })
-}
+// Right or wrong
+function checkAnswer(answer) {
 
-function resetState() {
-  clearStatusClass(document.body)
-  next-btn.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
+  var lineBreak = document.getElementById("lineBreak");
+  lineBreak.style.display = "block";
+  answerCheck.style.display = "block";
 
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+  if (questions[questionIndex].answer === questions[questionIndex].options[answer]) {
+      // correct +1
+      correctAns++;
+      console.log(correctAns);
+      document.getElementById(e.id).style.background= 'green';
+      answerCheck.textContent = "Correct!";
+  
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+      // Wrong -5 on the time
+      totalTime -= 5;
+      timeLeft.textContent = totalTime;
+      document.getElementById(e.id).style.background= 'red';
+      answerCheck.textContent = "Sorry, not correct, the answer is: " + questions[questionIndex].answer;
   }
-}
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
+  questionIndex++;
+  if (questionIndex < questions.length) {
+      nextQuestion();
   } else {
-    element.classList.add('wrong')
+      gameOver();
   }
 }
 
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+function chooseA() { checkAnswer(0); }
+
+function chooseB() { checkAnswer(1); }
+
+function chooseC() { checkAnswer(2); }
+
+function chooseD() { checkAnswer(3); }
+
+// Determines when the game is over.
+function gameOver() {
+  summary.style.display = "block";
+  questionDiv.style.display = "none";
+  startDiv.style.display = "none";
+  timer.style.display = "none";
+  timesUp.style.display = "block";
+
+  // Shows score
+  finalScore.textContent = correctAns;
 }
 
-const questions = [
-  {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
-    ]
-  },
-  {
-    question: 'Who is the best YouTuber?',
-    answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
-    ]
-  },
-  {
-    question: 'Is web development fun?',
-    answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
-    ]
-  },
-  {
-    question: 'What is 4 * 2?',
-    answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
-    ]
+// Enter initials
+// Stores highscore in local storage
+function storeHighScores(event) {
+  event.preventDefault();
+
+  // Stops if initial is blank
+  if (initialInput.value === "") {
+      alert("Please enter your initials!");
+      return;
+  } 
+
+  startDiv.style.display = "none";
+  timer.style.display = "none";
+  timesUp.style.display = "none";
+  summary.style.display = "none";
+  highScoreSection.style.display = "block";   
+
+  // Score into localStorage
+  var savedHighScores = localStorage.getItem("high scores");
+  var scoresArray;
+
+  if (savedHighScores === null) {
+      scoresArray = [];
+  } else {
+      scoresArray = JSON.parse(savedHighScores)
   }
-]
+
+  var userScore = {
+      initials: initialInput.value,
+      score: finalScore.textContent
+  };
+
+  console.log(userScore);
+  scoresArray.push(userScore);
+
+  var scoresArrayString = JSON.stringify(scoresArray);
+  window.localStorage.setItem("high scores", scoresArrayString);
+  
+  // Show current highscores
+  showHighScores();
 }
-)
+
+// Shows high scores
+var i = 0;
+function showHighScores() {
+
+  startDiv.style.display = "none";
+  timer.style.display = "none";
+  questionDiv.style.display = "none";
+  timesUp.style.display = "none";
+  summary.style.display = "none";
+  highScoreSection.style.display = "block";
+
+  var savedHighScores = localStorage.getItem("high scores");
+
+  // localStorage check
+  if (savedHighScores === null) {
+      return;
+  }
+  console.log(savedHighScores);
+
+  var storedHighScores = JSON.parse(savedHighScores);
+
+  for (; i < storedHighScores.length; i++) {
+      var eachNewHighScore = document.createElement("p");
+      eachNewHighScore.innerHTML = storedHighScores[i].initials + ": " + storedHighScores[i].score;
+      listOfHighScores.appendChild(eachNewHighScore);
+  }
+}
+
+// EventListeners
+startQuizBtn.addEventListener("click", newQuiz);
+choiceA.addEventListener("click", chooseA);
+choiceB.addEventListener("click", chooseB);
+choiceC.addEventListener("click", chooseC);
+choiceD.addEventListener("click", chooseD);
+
+submitInitialBtn.addEventListener("click", function(event){ 
+  storeHighScores(event);
+});
+
+viewHighScore.addEventListener("click", function(event) { 
+  showHighScores(event);
+});
+
+goBackBtn.addEventListener("click", function() {
+  startDiv.style.display = "block";
+  highScoreSection.style.display = "none";
+});
+
+clearHighScoreBtn.addEventListener("click", function(){
+  window.localStorage.removeItem("high scores");
+  listOfHighScores.innerHTML = "High Scores Cleared!";
+  listOfHighScores.setAttribute("style", "font-family: 'Archivo', sans-serif; font-style: italic;")
+});
