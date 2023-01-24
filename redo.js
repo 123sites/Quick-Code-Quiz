c375689c10d99eaaa082b124dcc59f060cf70f51
CF <cfreitas12345@gmail.com>
2023-01-24 12:36:17 -0700

    Redo is functioning better, needs styling.

--- redo.js
+++ redo.js
@@ -1,23 +1,25 @@
 // Directions***********************
-// This was a challenge where I had to create a multiple choice, 
-// timed code quiz.  It had to run in the browser. It needed to 
-// feature dynamically updated HTML and CSS, powered by JavaScript 
-// code.  There needed to be clean, polished, and responsive user 
+// This was a challenge where I had to create a multiple choice,
+// timed code quiz.  It had to run in the browser. It needed to
+// feature dynamically updated HTML and CSS, powered by JavaScript
+// code.  There needed to be clean, polished, and responsive user
 //interface.
 
+
 // The functions that it needed were:
-// 1) When the start button is clicked, the timer starts 
+// 1) When the start button is clicked, the timer starts
 // when presented with a question.
 // 2) Points subtracted when there is a wrong answer.
-// 3) When either all questions are answered or when the timer 
+// 3) When either all questions are answered or when the timer
 // reaches 0, the game is over.
-// 4) When the game is over, initials and score are saved. 
+// 4) When the game is over, initials and score are saved.
+
 
 // var
 var ques= document.getElementById('ques');
 var quizContainer= document.getElementById('container');
 var count = 3;
-var secondsLeft = 3;
+var secondsLeft = 10;
 var choice0= document.getElementById('choice0');
 var choice1= document.getElementById('choice1');
 var choice2= document.getElementById('choice2');
@@ -35,91 +37,115 @@ var timerEl = document.querySelector("#seconds");
 var timerElement = document.querySelector(".count");
 
 
+var ans = 0;
+var highScoreSection = document.getElementById("highScoreSection");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn")
+var viewHighScore = document.getElementById("viewHighScore");
+var highScores = document.getElementById("highScores");
 // Start
 var startBtn = document.querySelector("#startBtn");
-console.log('startBtn39') 
+console.log('startBtn39')
+
 
 // THIS MAKE THE START BUTTON WORK!  ALSO, MAKES ALERT BOX WORK!
 startBtn.addEventListener('click', ()=>{
   startTimer()
   quizContainer.classList.remove("hide")
-  console.log("startBtn47")
+
+
+  // console.log("startBtn47")
   // startBtn.classList.add("hide")
   // console.log("Hide startBtn")
-
+  // document.getElementById("startBtn").hidden=true;
+  // console.log("Hide now!")
 })
+
 
 function startBtn() {
   startTimer()
   currentQuestion = 0;
   showQuestion(dataArray[level])
-  startBtn.classList.add("hide");
-  console.log("Hide startBtn")
 
 
-
+  // const startBtn = document.getElementById('startBtn');
+  // startBtn.addEventListener('click', () => {
+  //   startBtn.remove();
+  // });
+  // console.log("RemoveBtn")
+ 
 // Timer code ------------------------------------------
   // Sets timer
-  var timeInterval = setInterval(function() {
-    count--;
+  // var timeInterval = setInterval(function() {
+  //   count--;
 
-    let = startTimer = 3;
-    seconds.textContent = count;
-    //timerElement = count;
-    if (count === 0) {
-      // Tests if won
-      //if (isWin && count > 0) {
-        // Stops timer & clears interval
-        clearInterval(timeInterval);
-        //winGame();
-        console.log('...finished in time')
-        alert('You finished in time!')
+
+  //   let = startTimer = 10;
+  //   seconds.textContent = count;
+
+
+
+
+  //   //timerElement = count;
+  //   if (count === 0) {
+  //       clearInterval(timeInterval);
+  //       console.log('...finished in time')
+  //       alert('You finished in time!')
+        // document.getElementById('incorrect').addEventListener('click', function() {
+        //   sec -= 5;
+        //   document.getElementById('startBtn').innerHTML='00:'+sec;
+        //   console.log("subtract 5 sec.")
+        // });
+        // startTimer();
+     
     }
  
-    // Time out?
-    if (count === 0) {
-      // Clears it
-      clearInterval(timeInterval);
-      alert('You ran out of time!');
-      // var stopInterval = function() {
-      //   console.log('Out of time!');
-      }
-    
-  }, 1000)
-}
+    // // Time out?
+    // if (count === 0) {
+    //   // Clears it
+    //   clearInterval(timeInterval);
+    //   alert('You ran out of time!');
+      // quizContainer.classList.remove("hide")
+      // function removeStart() {
+      //   document.getElementById("startBtn").classList.remove("btn");
+      // };
+      // const element = document.getElementById("startBtn");
+      // element.remove(startBtn);
 
-// function startTimer() {
-//   console.log("startTimer86")
-//   // Sets interval in variable
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timerEl.textContent = secondsLeft + " out of time!";
-//     console.log("timer")
-//     if(secondsLeft === 0) {
-//       // Stops it
-//       clearInterval(timerInterval);
-//     }
-//   }, 1000);
+
+     
+//       }
+//   }, 1000)
 // }
 
+
 // DO BELOW INSTEAD?????
-// Starts and stops the game.  It triggers win/lose
+// Starts and stops the game.  
 function startTimer() {
   // Sets timer
   console.log("startTimer")
   time = setInterval(function() {
     secondsLeft--;
     timerEl.textContent=secondsLeft + " seconds left until the time is up!";
+    function endGame (){
+      console.log("endOfGame")
+      localStorage.setItem("count", count);
+      console.log("localStorage")}
 
+
+
+   
     if (secondsLeft <=0) {
       clearInterval(time);
       endGame()
+      
+   
       // Tests if won
     //   if (isWin && secondsLeft > 0) {
     //     // Stops timer & clears interval
     //     clearInterval(time);
     //     // winGame();
     //   }
+
 
     // // Time out?
     // else {
@@ -131,20 +157,6 @@ function startTimer() {
   }, 1000);
 }
 
-// let seconds = startTimer;
-// function startTimer() {
-//   // Sets timer
-//   console.log("startTimer")
-//   // time = setInterval(function() {
-//     var seconds = 3;
-//     var timer= setInterval(function(){
-//       document.getElementById('timerDisplay').innerHTML='00:'+sec;
-//       sec--;
-//       if (sec < 0) {
-//         clearInterval(timer);
-//         alert("Out of time!")
-//       }
-//     )}, 1000;
 
 //function to display questions
 function displayQuestion(){
@@ -158,6 +170,7 @@ function displayQuestion(){
   option3.innerHTML= dataArray[i].option[3];
   stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+dataArray.length;
 }
+
 
 // Q & A
 console.log('dataArrayQuestions')
@@ -189,9 +202,10 @@ var dataArray= [
 }
 ]
 
-function endGame (){
-console.log("endOfGame")
-}
+
+
+
+
 // calculates scores
 function calcScore(e){
   if(e.innerHTML===dataArray[i].ans && score<dataArray.length)
@@ -205,6 +219,7 @@ function calcScore(e){
   setTimeout(nextQuestion,300);
 }
 
+
 // display next question
 function nextQuestion(){
   if(i<dataArray.length-1)
@@ -212,6 +227,7 @@ function nextQuestion(){
       i=i+1;
       displayQuestion();
       console.log(nextQuestion);
+     
   }
   else{
       points.innerHTML= score+ '/'+ dataArray.length;
@@ -220,13 +236,16 @@ function nextQuestion(){
   }
 }
 
+
 // Click events to next button
 next.addEventListener('click',nextQuestion);
+
 
 // Button event, returns to quiz
 function backToQuiz(){
   location.reload();
 }
+
 
 // Checks answers
 function checkAnswer(){
@@ -239,8 +258,10 @@ function checkAnswer(){
       var list= document.createElement('li');
       list.innerHTML= dataArray[a].ans;
       answers.appendChild(list);
+      correctAns++;
   }
 }
+
 
 // // Shows they won!
 // function inTime() {
@@ -249,6 +270,7 @@ function checkAnswer(){
 //   //startTimer.disabled = false;
 //   setWins()
 // }
+
 
 // // Shows they lost when the timer is 0.
 // function outOfTime() {
@@ -259,85 +281,38 @@ function checkAnswer(){
 // }
 
 
+
+
 // // Save score & intitials:
 // // Add data
 // localStorage.setItem('score');
 
+
 // // Read data
 // const score = localStorage.getItem('myScore');
+
 
 // // Remove specific data
 // localStorage.removeItem('myCar');
 
+
 // // Remove all data
 // localStorage.clear();
 
+
 displayQuestion();
 
-// const NO_OF_HIGH_SCORES = 10;
-// const HIGH_SCORES = 'highScores';
 
-// const highScoreString = localStorage.getItem(HIGH_SCORES);
-// const highScores = JSON.parse(highScoreString) ?? [];
-
-// const lowestScore = highScores[NO_OF_HIGH_SCORES — 1]?.score ?? 0
-
-// function checkHighScore(score) {
-//   const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
-//   const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
-  
-//   if (score > lowestScore) {
-//     saveHighScore(score, highScores); // TODO
-//     showHighScores(); // TODO
-//   }
-// }
-// function gameOver() {
-//   // Other game over logic.
-//   checkHighScore(account.score);
-
-//   const name = prompt(‘You got a high score! Enter name:’);
-
-//   const newScore = { score, name };
-
-  
-// function saveHighScore(score, highScores) {
-//   const name = prompt('You got a highscore! Enter name:');
-//   const newScore = { score, name };
-  
-//   // 1. Add to list
-//   highScores.push(newScore);
-
-//   // 2. Sort the list
-//   highScores.sort((a, b) => b.score - a.score);
-  
-//   // 3. Select new list
-//   highScores.splice(NO_OF_HIGH_SCORES);
-  
-//   // 4. Save to local storage
-//   localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
-// };
-
-// <h2>HIGH SCORES</h2>
-// <ol id=”highScores”></ol>
-
-// highScores.map((score) => `<li>${score.score} — ${score.name}`);
+submitInitialBtn.addEventListener("click", function(event){
+  storeHighScores(event);
+});
 
 
-// const highScoreList = document.getElementById(HIGH_SCORES);
-
-// highScoreList.innerHTML = highScores.map((score) => 
-//   `<li>${score.score} - ${score.name}`
-// );
+viewHighScore.addEventListener("click", function(event) {
+  showHighScores(event);
+});
 
 
-// function showHighScores() {
-//   const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
-//   const highScoreList = document.getElementById(HIGH_SCORES);
-  
-//   highScoreList.innerHTML = highScores
-//     .map((score) => `<li>${score.score} - ${score.name}`)
-//     .join('');
-// }
 
-// }
+
 
